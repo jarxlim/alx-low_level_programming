6940d3ea75fd8d84c6c51e2e05477d3e93f7effe
chima chike <chimachike1@gmail.com>
2023-02-18 05:54:54 -0800

    2-print_alphabet git commit -m 2-print_alphabet clear git commit -m 2-print_alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char b;
+	char c;
+
+	b = 'a';
+	c = 'A';
+	while (b <= 'z')
+	{
+		putchar(b);
+		b++;
+	}
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
