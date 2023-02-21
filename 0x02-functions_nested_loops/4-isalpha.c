48cd6f6d6e17a162e048d2de119f7af7a2aff36c
chima chike <chimachike1@gmail.com>
2023-02-21 15:31:33 -0800

    4 git commit -m 4-isalpha.c git commit -m 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,16 @@
 #include "main.h"
-/** 5-sign - Entry point
+/** _isalpha - Entry point
  * description: a function that checks for alphabetic character.
- * return: Returns 1 if c is a letter, lowercase or uppercase, returns 0 otherwise
+ * return: Returns 1 if c is a letter, lowercase or uppercase, returns 0 if not
  */
 
-
+int _isalpha(int c)
+{
+	if (c == 'a' || c == 'A' || c == 'z' || c == 'Z')
+	{
+		return (1);
+	}
+	{
+		return (0);
+	}
+}
