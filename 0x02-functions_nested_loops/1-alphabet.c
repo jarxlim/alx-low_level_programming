43ebe192e5affdb2289beb495b4854e9e393bb46
chima chike <chimachike1@gmail.com>
2023-02-21 13:58:57 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * print_alphabet: prints the alphabet, in lowercase
+ * print_alphabet - prints the alphabet, in lowercase
  * Return: Always 0 (Success).
  */
 void print_alphabet(void)
