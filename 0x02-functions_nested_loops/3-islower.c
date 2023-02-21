ddd7c2ae22f2e43a9fc2dd05d10eddd89c39b786
chima chike <chimachike1@gmail.com>
2023-02-21 14:50:40 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - checks for alphabetic character.
+ * Description: checks is a character is lowercase
+ * Return: 1 if true. 0 if false.
+ */
+
+int alpha
+
+int _islower(void)
+{
+	if (alpha >= 'a' && alpha <= 'z')
+	{
+		return (1);
+	}
+	{
+		return (0);
+	}
+}
