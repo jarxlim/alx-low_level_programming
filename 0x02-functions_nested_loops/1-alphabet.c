fbd1a47b4a59c3664a3b9d5618008c2e41f21495
chima chike <chimachike1@gmail.com>
2023-02-21 13:51:13 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * description: prints the alphabet, in lowercase, followed by a new line.
+ * Return: Always 0 (Success).
+ */
+void print_alphabet(void)
+{
+
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+			_putchar('\n');
+}
