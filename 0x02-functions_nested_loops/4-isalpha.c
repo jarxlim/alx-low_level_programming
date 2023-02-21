849df940c157d230c68f7a24a7ca8e7a090ce35c
chima chike <chimachike1@gmail.com>
2023-02-21 15:48:30 -0800

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,18 @@
 #include "main.h"
-/** _isalpha - Entry point
+/**
+ * _isalpha - Entry point
  * description: a function that checks for alphabetic character.
+ * @c: the integer value it receives
  * return: Returns 1 if c is a letter, lowercase or uppercase, returns 0 if not
  */
 
 int _isalpha(int c)
 {
-	if (c == 'a' || c == 'A' || c == 'z' || c == 'Z')
+       	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
+	else
 	{
 		return (0);
 	}
