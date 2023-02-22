68e08184ef411ccdeae6e3e16a79805a321ce016
chima chike <chimachike1@gmail.com>
2023-02-21 18:23:01 -0800

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,11 +8,13 @@
 void times_table(void)
 {
 	int i, j;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			int result = i * j;
+
 			if (result > 9)
 			{
 				_putchar(result / 10 + '0');
