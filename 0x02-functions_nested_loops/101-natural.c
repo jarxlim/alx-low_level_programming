7ab52e6b216bb4da8c8ae62e0ac36089fb71b8af
chima chike <chimachike1@gmail.com>
2023-02-22 07:07:20 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,15 +7,16 @@
 int main(void)
 {
 	int sum = 0;
-
-	for (int i = 1; i < 1024; i++)
+	int i;
+	
+	while (i < 1024)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
 			sum += i;
 		}
-		{
-		}
+		i++
+	}
 		_putchar('\n');
 		return (0);
 
