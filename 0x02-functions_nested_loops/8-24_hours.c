c865a7026778f066061908a296b0daca1746dff9
chima chike <chimachike1@gmail.com>
2023-02-21 18:54:42 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,40 +1,45 @@
+#include "main.h"
 /**
- * jack_bauer - prints every minute of the day of jack bauer
- * Description: prints every minute of the day of jack bauer
+ * jack_bauer - prints every minute of the day
+ * Description: prints every minute of the day
  * Return: void
  */
 void jack_bauer(void)
 {
-	int a, b;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-	for (a = 0; a < 24; a++)
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
 	{
-		for (b = 0; b < 60; b++)
+		if (hours_tens == '2')
 		{
-			if (a < 10)
-			{
-				_putchar('0');
-				_putchar(a + '0');
-			}
-			else if (a >= 10)
-			{
-				_putchar((a / 10) + '0');
-				_putchar((a % 10) + '0');
-			}
-			if (b < 10)
-			{
-				_putchar(':');
-				_putchar('0');
-				_putchar(b + '0');
-			}
-			else if (b >= 10)
-			{
-				_putchar(':');
-				_putchar((b / 10) + '0');
-				_putchar((b % 10) + '0');
-			}
-			_putchar('\n');
+			hours_max = '4';
 		}
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_tens = '0';
+			while (minutes_tens < '6')
+			{
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++;
 	}
 }
-
