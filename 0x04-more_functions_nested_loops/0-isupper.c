802ed3bc32725e5e1df394a7034371198ea7cc49
chima chike <chimachike1@gmail.com>
2023-02-23 12:31:02 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: a function that checks for uppercase character.
+ * Return: 1 if c is uppercase and 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+	while (c >= 'A')
+	{
+		if (c = 'A')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,7 @@
+This project nested loops and how to use them
+Function and how to use functions
+The difference between a declaration and a definition of a function
+Prototypes
+Scope of variables
+The gcc flags -Wall -Werror -pedantic -Wextra -std=gnu89
+What header files are and how to to use them with #include
