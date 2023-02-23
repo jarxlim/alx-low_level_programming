ed8cf2da549d89d62efafaa3475728234cc345dc
chima chike <chimachike1@gmail.com>
2023-02-23 14:35:17 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * 3-print_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_numbers(void)
+{
+	int k;
+
+	for (k = '0'; k <= '9'; k++)
+	{
+		_putchar("%d\n", k)
+	}
+	return (0)
+}
