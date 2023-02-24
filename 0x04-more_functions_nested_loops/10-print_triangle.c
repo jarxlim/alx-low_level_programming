a82bba8683ce7cf61b5a60b30eb3a3a473f8328f
chima chike <chimachike1@gmail.com>
2023-02-24 11:05:52 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,19 +5,29 @@
  * @size : integer
  * Return: always 0
  */
+
 void print_triangle(int size)
 {
-	if (size <= 0)
+	int i = 1, ii;
+
+	while (i <= size && size > 0)
 	{
-		_putchar('\n');
-		return;
-	}
-	for (int i = 1; i <= size; i++)
-	{
-		for (int j = 1; j <= i; j++)
+		ii = 0;
+		while (ii < size - i)
+		{
+			_putchar(' ');
+			ii++;
+		}
+		ii = 0;
+		while (ii < i)
 		{
 			_putchar('#');
+			ii++;
 		}
+
 		_putchar('\n');
+		i++;
 	}
+	if (i == 1)
+		_putchar('\n');
 }
