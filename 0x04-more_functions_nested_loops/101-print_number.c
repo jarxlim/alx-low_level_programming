55333dc7a1e6c634b865944f76827f9c3ad62d19
chima chike <chimachike1@gmail.com>
2023-02-24 11:36:12 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,14 +7,17 @@
 
 void print_number(int n)
 {
+	unsigned int t;
+
+	t = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		t = -n;
 	}
-	if (n >= 10)
+	if (t >= 10)
 	{
-		print_number(n / 10);
+		print_number(t / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(t % 10 + '0');
 }
