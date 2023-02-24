1a55234f4bed79fc80da60005c71479ea4e4f612
chima chike <chimachike1@gmail.com>
2023-02-23 17:38:11 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point
+ * Return: Always o
+ */
+
+int main(void)
+{
+	int t;
+
+	for (t = 1; t <= 100; t++)
+	{
+		if (t % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (t % 3 == 0 && t % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (t % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%c ", t);
+		}
+	}
+	printf('\n');
+}
