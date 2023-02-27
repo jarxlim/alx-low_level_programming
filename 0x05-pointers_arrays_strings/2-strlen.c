45e6700bdfdd4ddda9790591f81fe14098989363
chima chike <chimachike1@gmail.com>
2023-02-27 14:48:52 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: a string
+ * Return: string length
+ */
+int _strlen(char *s)
+{       
+        len = strlen(*s);
+        _putchaar('\n);
+        return (0);
+}       
+~      
