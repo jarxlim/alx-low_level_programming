8cdc90a3f7b5577cb77a1e19c4d969d952196556
chima chike <chimachike1@gmail.com>
2023-02-27 18:04:48 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,25 +6,15 @@
  */
 void puts_half(char *str)
 {
-	int len, i, n;
+	int i;
 
-	len = 0;
-	while (str[len] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		len++;
+	i++;
 	}
-	if (len % 2 == 0)
+	for (i /= 2; str[i] != '\0'; i++)
 	{
-		for (i = len / 2; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-	}else if (len % 2)
-	{
-		for (n = (len - 1) / 2; n <  len - 1; i++)
-		{
-			_putchar(str[n + 1]);
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
