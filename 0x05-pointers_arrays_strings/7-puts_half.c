4a71d3da97bb65dd46110fde579d69048ebe27bd
chima chike <chimachike1@gmail.com>
2023-02-27 18:01:16 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * puts_half - a function that prints half of a string, 
- * @str; string to print
+ * puts_half - a function that prints half of a string,
+ * @str: string to print
  * Return: void
  */
 void puts_half(char *str)
@@ -19,10 +19,9 @@ void puts_half(char *str)
 		{
 			_putchar(str[i]);
 		}
-	}
-	else if (len % 2)
+	}else if (len % 2)
 	{
-		for (n = (len -1) / 2; n <  len -1; i++)
+		for (n = (len - 1) / 2; n <  len - 1; i++)
 		{
 			_putchar(str[n + 1]);
 		}
