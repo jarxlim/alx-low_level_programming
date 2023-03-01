963548a7c0021535ac316a000a338203117a98d0
chima chike <chimachike1@gmail.com>
2023-03-01 09:38:26 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcat - a function that concatenates two strings.
+ * @dest: string where src string appends to
+ * @src: string that appends to the dest string
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		for (j = 0; src[j] != '\0'; j++)
+		{
+			dest[i] = src[j];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+In this projec, i continued worked on:
+Pointers and how to use them
+Arrays and how to use them
+Differences between pointers and arrays
+How to use strings and how to manipulate them and
+Scope of variables
