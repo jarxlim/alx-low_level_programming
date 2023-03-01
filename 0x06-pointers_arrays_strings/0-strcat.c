bc234718304d3a398972ffa887b6b20317be1bfa
chima chike <chimachike1@gmail.com>
2023-03-01 10:18:34 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,14 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; dest[i]; i++)
-	{
-		for (j = 0; src[j] != '\0'; j++)
+	i = 0;
+	while (dest[i])
+		i++;
+	for (j = 0; src[j] != '\0'; j++)
 		{
 			dest[i] = src[j];
 			i++;
 		}
 		dest[i] = '\0';
 		return (dest);
-	}
 }
