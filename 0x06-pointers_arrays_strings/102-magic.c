220684d9d4186bee9d14365d01904733e67ff857
chima chike <chimachike1@gmail.com>
2023-03-02 14:49:43 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,1 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * return: Always 0
+ */
 
+int main(void)
+{
+	int i; a[], *p;
+	
+	a[2] = 1024;
+	p = &i;
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
