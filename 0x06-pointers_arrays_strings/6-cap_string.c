04f4fd75028e2421dec7a8a167566acf3edce3ae
chima chike <chimachike1@gmail.com>
2023-03-02 09:55:13 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,22 @@
 #include "main.h"
 /**
  * cap_string - a function that capitalizes all words of a string.
- *
+ * @str: string to be capitalized
  * Return: capitalized first words
  */
 char *cap_string(char *str)
 {
-	int i, len;
-	int word_start = 1;
-	
-	for (len = 0; str[len] != '\0'; len++)
-	{
-		for (i = 0; i < len; i++) 
-		{
-			if (word_start && (str[i] >= 'a' && str[i] <= 'z'))
-			{
-				str[i] -= 32;
-				word_start = 0;
-			}
-			else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-			{
-				word_start = 1;
-			}
-		}
+	int i;
+
+	i = 0;
+	while (str[i])
+        {
+		i++;
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || i == 0)
+			i++;
+		str[i] = str[i] - 32;
 	}
 	return str;
 }
