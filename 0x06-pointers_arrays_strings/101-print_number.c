1b04cac4284f2f1b2c79e2864548c8abbd6b4d3d
chima chike <chimachike1@gmail.com>
2023-03-02 14:37:25 -0800

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - 
+ * @n: intger to be printed
+ * Return: an integer
+ */
+void print_number(int n)
+{
+	unsigned int numb = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		numb = -numb;
+	}
+	if ((numb / 10) > 0)
+		print_number(numb / 10);
+	_putchar((numb % 10) + '0');
+}
