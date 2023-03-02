7f3aae92aa193794f64538d6c77fdd66d4adffeb
chima chike <chimachike1@gmail.com>
2023-03-02 10:06:06 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,18 +6,31 @@
  */
 char *cap_string(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (str[i])
-        {
-		i++;
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
+	{
+		while (!(str[i] >= 'a' && str[index] <= 'z'))
 			i++;
-		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || i == 0)
-			i++;
-		str[i] = str[i] - 32;
-	}
-	return str;
-}
 
+		if (str[i - 1] == ' ' ||
+		    str[i - 1] == '\t' ||
+		    str[i - 1] == '\n' ||
+		    str[i - 1] == ',' ||
+		    str[i - 1] == ';' ||
+		    str[i - 1] == '.' ||
+		    str[i - 1] == '!' ||
+		    str[i - 1] == '?' ||
+		    str[i - 1] == '"' ||
+		    str[i - 1] == '(' ||
+		    str[i - 1] == ')' ||
+		    str[i - 1] == '{' ||
+		    str[i - 1] == '}' ||
+		    i == 0)
+			str[i] -= 32;
+
+		i++;
+	}
+
+	return (str);
+}
