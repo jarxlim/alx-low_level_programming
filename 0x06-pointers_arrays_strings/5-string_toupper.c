3638684c1d81f2684fc4470f6bf16ab068872281
chima chike <chimachike1@gmail.com>
2023-03-02 05:01:28 -0800

    5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "string.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ *
+ * Return: uppercase letters
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+
+		i++;
+	}
+	return (str);
+}
