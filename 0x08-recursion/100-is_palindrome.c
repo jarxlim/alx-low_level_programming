3f0897673a6660629257b7861b1421b3aac42b43
chima chike <chimachike1@gmail.com>
2023-03-08 05:50:25 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,21 +1,56 @@
 #include "main.h"
-#include <string.h>
 /**
- * is_palindrome - function returns 1 if string palindrome and 0 if not.
- * @s: string to be checked
- * Return: 1 if palindrome, 0 if its not.
+ * str_length - length of a string.
+ * @s: string to check the lenght.
+ * Return: length of the string.
+ */
+int str_length(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len = len + str_length(s + len);
+	}
+
+	return (len);
+}
+
+/**
+ * palind_check - checks if a string is a palindrome.
+ * @s: string to be checked.
+ * @len: length of the string.
+ * @i: Index of the string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ * If the string is not a palindrome - 0.
+ */
+int palind_check(char *s, int len, int i)
+{
+	if (s[i] == s[len / 2])
+		return (1);
+
+	if (s[i] == s[len - i - 1])
+		return (palind_check(s, len, i + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ * If the string is not a palindrome - 0.
  */
 int is_palindrome(char *s)
 {
-	int i;
-	int len = strlen(s);
+	int i = 0;
+	int len = str_length(s);
 
-	for (i = 0; i < len / 2; i++)
-	{
-		if (s[i] != s[len - i - 1])
-		{
-			return (0);
-		}
-	}
-	return (1);
+	if (!(*s))
+		return (1);
+
+	return (palind_check(s, len, i));
 }
