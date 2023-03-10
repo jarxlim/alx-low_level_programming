7596da573c7188ff23ea5534bdb44beb15bc04a6
chima chike <chimachike1@gmail.com>
2023-03-10 13:24:45 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - a program that prints all arguments it receives.
+ * @argv: array containing the command-line arguments
+ * @argc: number of the command-line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
