19229e6bc15f722cd9e6bce4610b337035e2fce7
chima chike <chimachike1@gmail.com>
2023-03-10 14:30:16 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,10 +10,10 @@
 int main(int argc, char *argv[])
 {
 	int num1 = atoi(argv[1]);
-        int num2 = atoi(argv[2]);
-        int result = num1 * num2;
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2;
 
-	if (argc != 3)
+	if (argc == 1 || argc == 2)
 	{
 		printf("Error\n");
 		return (1);
