9ad5378f59aa666a7ee9ea441eeb1be31f4fca33
chima chike <chimachike1@gmail.com>
2023-03-10 12:52:30 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argv: array containing the command-line arguments
+ * @argc: number of the command-line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+In this project i worked on:
+How to use arguments passed to my program
+Two prototypes of main that I know of, and in which case to use one or the other
+How to use __attribute__((unused)) or (void) to compile functions with unused variables or parameters
