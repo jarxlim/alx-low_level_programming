1c38dc44330b6940f4db889fe865e8629a7c5b5f
chima chike <chimachike1@gmail.com>
2023-03-20 13:51:03 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - a type describing a dog
+ * @name: name of the dog
+ * @age: dog's age
+ * @owner: name of the dog's owner
+ */
 struct dog
 {
 	char *name;
