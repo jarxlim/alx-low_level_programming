780a409a2b71c744e893516e6753f882cb012406
chima chike <chimachike1@gmail.com>
2023-03-20 06:01:30 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - program that prints the name of the file it was compiled from
+ *
+ * Return: 0
+ */
 
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
