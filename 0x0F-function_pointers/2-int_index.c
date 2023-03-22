b23992241f94295b87db2dc66aa05b34f3a60cb6
chima chike <chimachike1@gmail.com>
2023-03-22 14:47:57 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,13 +10,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size && cmp)
 	{
 		return (-1);
 	}
 	for (i = 0; i < size; i++)
 	{
-		if ((*cmp)(array[i]))
+		if ((*cmp)(array[i]) != 0)
 		{
 			return (i);
 		}
