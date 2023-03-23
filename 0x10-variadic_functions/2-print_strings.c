9b59c0256c14938341aa36adf521f70ba3e358ae
chima chike <chimachike1@gmail.com>
2023-03-23 15:04:08 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str =  va_arg(strings, char *);
 
+		if (str == NULL)
+			printf("(nil)");
 		printf("%s", str);
+		
 		if (separator != NULL && i != n - 1)
 		{
 			printf("%s", separator);
