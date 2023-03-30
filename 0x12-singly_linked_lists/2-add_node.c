6271b303598ffa94b89394e627fd05044725ae80
chima chike <chimachike1@gmail.com>
2023-03-30 12:45:36 -0700

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,20 +1,12 @@
 #include "lists.h"
 #include <string.h>
-
+#include <stdlib.h>
 /**
  * add_node - function that adds a new node at the beginning of a list_t list.
  * @head: head of the linked list
  * @str: a string parameter
  * Return: NULL, if funtion fails or else returns a pointer to the new node
  */
-
-typedef struct list_s
-{
-	char *str;
-	struct list_s *next;
-}
-list_t;
-
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
