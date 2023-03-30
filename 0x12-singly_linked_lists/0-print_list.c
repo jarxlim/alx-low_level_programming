9fd81b957ddd1c640ad2588a6b25758ca0b575ec
chima chike <chimachike1@gmail.com>
2023-03-30 09:25:23 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,11 +8,12 @@
  *
  * Return: The number of nodes in the list
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t node = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
