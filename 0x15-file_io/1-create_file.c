d27233c298cd08999a514a5b8f71b29e4e41f9b5
chima chike <chimachike1@gmail.com>
2023-04-07 07:56:16 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * @filename: the name of the file to create
  * @text_content: is a NULL terminated string to write to the file
  * Return: 1 on success, -1 on failure
- * (file can not be created, file can not be written, write “fails”, etc…)
+ * (file can't be created, file can not be written, write “fails”, etc…)
  */
 
 int create_file(const char *filename, char *text_content)
@@ -34,7 +34,6 @@ int create_file(const char *filename, char *text_content)
 		}
 		if (wr == -1 || wr != len)
 		{
-			close(opn);
 			return (-1);
 		}
 	}
