6830c2f270aff6531adc0d8e301cb5dfe1e34e75
chima chike <chimachike1@gmail.com>
2023-04-07 08:07:45 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len] != '\0'; len++)
+		for (len = 0; text_content[len] != '\0';)
+			len++;
 	}
 	else
 	{
