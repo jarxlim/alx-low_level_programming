ec24a1653f6d4dd9a698c1d50a5146c34da5cc5c
chima chike <chimachike1@gmail.com>
2023-04-09 04:48:18 -0700

    fixed issue

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,7 +2,7 @@
 /**
  * get_endianness - function that checks the endianness
  *
- * Returns: 0 if big endian, 1 if little endian
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
