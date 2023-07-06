07d533e254685a94ce58e32713ac63ae8363c32e
chimachike1@gmail.com <chimachike1@gmail.com>
2023-07-06 10:08:06 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,6 +5,7 @@
  * @size: size of the arr
  * Return: a pointer to the new hash table
  */
+hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
 	unsigned long int i;
